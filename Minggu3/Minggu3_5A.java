ab054aac8e54fe0f7e7c1aefcbd998cad40194df
Danu-Wicaksono <80568927+Danu-Wicaksono@users.noreply.github.com>
2021-03-23 15:21:53 +0700

    Add files via upload

---
+++ Minggu3/Minggu3_5A.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minggu3_5a;
+
+/**
+ *
+ * @author Danuw
+ */
+public class Minggu3_5A {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         String [] nama = {"Danu", "Wicaksono"};
+        
+        for (String nama1 : nama) {
+            System.out.print(nama1);
+            System.out.print(" ");
+        } 
+}}
