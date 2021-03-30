67ef1e8bbe9873f80c6c90aa36a58b3e6ed7d125
Danu-Wicaksono <80568927+Danu-Wicaksono@users.noreply.github.com>
2021-03-30 16:58:01 +0700

    Add files via upload

---
+++ Minggu 3/Minggu3_5B.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minggu3_5b;
+
+/**
+ *
+ * @author Danuw
+ */
+public class Minggu3_5B {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int [] angka = {76,69,11,4646,123,666};
+        
+        for (int i=0; i<angka.length; i++){
+            System.out.println(angka[i]);
+        
+    }
+    
+}}
+
